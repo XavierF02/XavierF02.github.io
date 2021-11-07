202b894bb07da9e6b2108cc2f0e4ece1b8634fc4
XavierF02 <61722434+XavierF02@users.noreply.github.com>
2021-11-07 16:32:41 -0500

    Completed Activity 10

--- itis3135/exercise/Faulkner-Activity10.js
+++ itis3135/exercise/Faulkner-Activity10.js
@@ -2,13 +2,38 @@ $(document).ready(function() {
 
 
         // preload the image for each link
+        $("#image_list a").each(function() { 
+                var swappedImage = new Image(); 
+                swappedImage.src = $(this).attr("href"); 
+        }); 
 
         // set up the event handlers for each link
+        $("#image_list a").click(function(evt) { 
+                // get the image URL and caption for each image and animate the caption
+                var imageURL = $(this).attr("href");
+                var caption = $(this).attr("title");
+                $("#image").fadeOut(3000, 
+                        function() {
+                                $(this).attr("src", imageURL);
+                                $(this).fadeIn(3000);
+                        }
+                );
+                $("#caption").fadeOut(3000, 
+                        function() {
+                                $(this).text(caption);
+                                $(this).fadeIn(3000,
+                                        function() {
+                                                $(this).delay(3000).animate(
+                                                        {fontSize: "2em"}
+                                                );
+                                        }
+                                );
+                        }
+                );
 
-    			// get the image URL and caption for each image and animate the caption
-
-            // cancel the default action of each link
-
+                // cancel the default action of each link 
+                evt.preventDefault();
+        });
 
     // move the focus to the first link
 
